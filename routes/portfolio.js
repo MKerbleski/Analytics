304ad90754cfcb8263fc9cb183e4f5d2068356e7
mkerbleski <mkerbles@gmail.com>
2019-09-08 14:12:45 -0400

    deleted console logs

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -36,13 +36,9 @@ function getSession(uuid){
 router.post('/', async (req, res, next) => {
     getSession(req.body.session.uuid).then(priorSession => {
         if(priorSession){
-            console.log('priorSession', priorSession)
             let oldData = priorSession.data
-            console.log('oldData', oldData)
             let oldTracker = oldData.tracker
-            console.log('oldTracker', oldTracker)
             req.body.tracker.unshift(...oldTracker)
-            console.log('req.body', req.body)
                 updateData(req.body).then(response => {
                     console.log('user session update')
                     res.status(200).json({ message: 'data update'})
